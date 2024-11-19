74cd2ef80c77f6b2ea2a27bc2b3f390a2a59421b
SheaMcCann <154007920+SheaMcCann@users.noreply.github.com>
2024-11-19 14:40:11 -0500

    Reapply "Merge pull request #1 from vienna-hev/pinpoint"
    This reverts commit 879a319202a24ea098daf5515616773f90a92fd7.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class AutoTest extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}

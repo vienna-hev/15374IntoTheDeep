c017b440d31bc7caa901157799f1b4be5e75831d
SheaMcCann <154007920+SheaMcCann@users.noreply.github.com>
2024-11-21 16:24:32 -0500

    Update sheaTeleOp.java
    intake working, other stuff, day before scrimmage

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sheaTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sheaTeleOp.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
         import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
         import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+        import com.qualcomm.robotcore.hardware.CRServo;
         import com.qualcomm.robotcore.hardware.DcMotor;
         import com.qualcomm.robotcore.hardware.DcMotorSimple; //???
         import com.qualcomm.robotcore.hardware.Servo;
@@ -9,28 +10,35 @@ package org.firstinspires.ftc.teamcode;
 @TeleOp
 
 public class sheaTeleOp extends LinearOpMode {
-
+int num;
     public DcMotor frontRight;
     public DcMotor frontLeft;
     public DcMotor backLeft;
     public DcMotor backRight;
-    public Servo intakeLeft;
-    public Servo intakeRight;
+    public DcMotor upMotor;
+    public CRServo intakeLeft;
+    public CRServo intakeRight;
+    public Servo wrist;
+    public Servo elbow;
+    public Servo bucketL;
+    public Servo bucketR;
 
     @Override
     public void runOpMode() {
 
         frontRight = hardwareMap.get(DcMotor.class, "right_front");
-
         frontLeft = hardwareMap.get(DcMotor.class, "left_front");
-
         backLeft = hardwareMap.get(DcMotor.class, "left_back");
-
         backRight = hardwareMap.get(DcMotor.class, "right_back");
 
-        intakeLeft = hardwareMap.get(Servo.class, "intakeServoL");
+        upMotor = hardwareMap.get(DcMotor.class, "upMotor");
 
-        intakeRight = hardwareMap.get(Servo.class, "intakeServoR");
+        intakeLeft = hardwareMap.get(CRServo.class, "intakeServoL");
+        intakeRight = hardwareMap.get(CRServo.class, "intakeServoR");
+        wrist = hardwareMap.get(Servo.class, "wristServo");
+        elbow = hardwareMap.get(Servo.class, "elbowServo");
+        bucketL = hardwareMap.get(Servo.class, "bucketServoL");
+        bucketR = hardwareMap.get(Servo.class, "bucketServoR");
 
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -108,8 +116,50 @@ public class sheaTeleOp extends LinearOpMode {
                 backRight.setPower(0);
             }
 
-            if (gamepad1.dpad_left){
-                intakeLeft.setPower(.5);
+            if (gamepad2.dpad_left){
+                intakeLeft.setPower(-.5);
+                intakeRight.setPower(.5);
+            }
+            else if (gamepad2.dpad_right){
+                intakeLeft.setPower(.3);
+                intakeRight.setPower(-.3);
+            }
+            else {
+                intakeLeft.setPower(0);
+                intakeRight.setPower(0);
+            }
+
+            if (gamepad2.left_stick_y> 0.9){
+                wrist.setPosition(.30);
+//                num = 1;
+            }
+            else if (gamepad2.left_stick_y< 0.9){
+                wrist.setPosition(.80);
+//                num =2;
+            }
+//            else{
+//                if (num == 1){
+//                    wrist.setPosition(.85);
+//                }
+//                else if (num == 2){
+//                    wrist.setPosition(.25);
+//                }
+//                else {
+//                    wrist.setPosition(0);
+//                }
+//            }
+            if (gamepad2.right_stick_x > 0.9){
+                elbow.setPosition(0.80);
+            }
+            else{
+                elbow.setPosition(0.40);
+            }
+
+            if (gamepad2.y){
+                upMotor.setPower(-0.5);
+            }
+            else{
+                upMotor.setPower(0);
             }
 
             double speed = gamepad1.left_stick_y;

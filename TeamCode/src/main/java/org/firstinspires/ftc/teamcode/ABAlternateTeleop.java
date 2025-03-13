55b22c35f95048de0053a0a5e1dd30902654eab8
Reed Kutcher <reed_kutcher@students.nksd.net>
2025-03-13 16:15:10 -0400

    Updated Alternate Controls
    They're weird now but should accommodate the new intake extension functionality.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ABAlternateTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ABAlternateTeleop.java
@@ -15,17 +15,18 @@ public class ABAlternateTeleop extends LinearOpMode {
     @Override
     public void runOpMode() {
         hardware = new TOHardware(hardwareMap);
+        double pwrDec;
 
         waitForStart();
 
         while (opModeIsActive()) {
-            hardware.funcIntake(gamepad2.a, gamepad2.b);
-            hardware.funcWrist(gamepad2.right_bumper);
-            hardware.funcExtend(gamepad2.right_trigger);
-            hardware.funcLift(gamepad2.dpad_up, gamepad2.dpad_down, gamepad2.left_trigger, gamepad2.left_bumper);
+            hardware.funcIntake(gamepad2.right_bumper, gamepad2.left_bumper);
+            hardware.funcWrist(gamepad2.b);
+            hardware.funcExtend(gamepad2.right_trigger - gamepad2.left_trigger);
+            pwrDec = Math.hypot(gamepad2.left_stick_x, gamepad2.left_stick_y);
+            hardware.funcLift(gamepad2.y, gamepad2.a, pwrDec, gamepad2.left_stick_button);
             hardware.funcBucket(gamepad2.x);
-            hardware.funcDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x + gamepad1.right_trigger - gamepad1.left_trigger,
-                    1.0, 1.0, 0.6);
+            hardware.funcDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x + gamepad1.right_trigger - gamepad1.left_trigger, 1.0, 1.0, 0.6);
         }
     }
 }

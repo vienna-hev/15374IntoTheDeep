cc7a6d32a1e1b6280b4f2a7f2a2bbc324cbd42a8
Reed Kutcher <reed_kutcher@students.nksd.net>
2025-03-15 15:35:26 -0400

    Alternate Controls are a little better

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ABAlternateTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ABAlternateTeleop.java
@@ -22,7 +22,7 @@ public class ABAlternateTeleop extends LinearOpMode {
         while (opModeIsActive()) {
             hardware.funcIntake(gamepad2.right_bumper, gamepad2.left_bumper);
             hardware.funcWrist(gamepad2.b);
-            hardware.funcExtend(gamepad2.right_trigger - gamepad2.left_trigger);
+            hardware.funcExtend(gamepad2.left_trigger - gamepad2.right_trigger);
             pwrDec = Math.hypot(gamepad2.left_stick_x, gamepad2.left_stick_y);
             hardware.funcLift(gamepad2.y, gamepad2.a, pwrDec, gamepad2.left_stick_button);
             hardware.funcBucket(gamepad2.x);
